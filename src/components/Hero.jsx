2770bd8f414f9127f78097b3813af6f68071f027
jsdodiya <jinendradodiya9@gmail.com>
2025-02-04 16:33:44 +0530

    insights

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,29 +1,84 @@
-import React from 'react'
+import React from "react";
 
 import { IoPlay } from "react-icons/io5";
-
+import { delay, motion } from "framer-motion";
 
 const Hero = () => {
+
+    const textVariant = {
+        hidden:{opacity:0, x: -100},
+        visible:{
+            opacity:1,
+            x:0,
+            transition:{
+                duration:1.0,
+                ease: "easeInOut",
+            }
+        }
+    }
+
+
+    const buttonVariant = {
+        hidden:{opacity:0},
+        visible:{
+            opacity:1,
+            transition:{
+                delay: 0.5,
+                duration: 1.0,
+            }
+        }
+    }
+
+    const imageVariant = {
+        hidden:{opacity:0, x: 50},
+        visible:{
+            opacity:1,
+            x:0,
+            transition:{
+                duration:1.0,
+                ease: "easeInOut",
+            }
+        }
+    }
+
   return (
-    <div>
-        <div>
-        <p>___ Innovation Requires Courages</p>
-        <h1>Being Innovatores Requiares Courage</h1>
-        <p>
-            Understanding your capacity for risk can guide your business
-            into untapped opportuinities, helping you navigate growth
-        </p>
-        <div>
-            <button>Book</button>
-            <button> <IoPlay /> <span>Video </span></button>
+    <div className="w-4/5 py-20 m-auto flex flex-col md:flex-row justify-between items-center space-y-10 md:space-y-0">
+      <div className="md:w-1/2 space-y-6">
+        <motion.p initial="hidden" whileInView="visible" viewport={{amount:0.8}} variants={textVariant} >___ Innovation Requires Courages</motion.p>
+        <motion.h1 initial="hidden" whileInView="visible" viewport={{amount:0.8}} variants={textVariant} className="text-5xl md:text-7xl font-bold">
+          Being Innovatores Requiares Courage
+        </motion.h1>
+        <motion.p initial="hidden" whileInView="visible" viewport={{amount:0.8}} variants={textVariant} className="md:text-lg">
+          Understanding your capacity for risk can guide your business into
+          untapped opportuinities, helping you navigate growth
+        </motion.p>
+        <div className="flex space-x-5">
+          <motion.button initial="hidden" whileInView="visible" viewport={{amount:0.8}} variants={buttonVariant} className="px-5 py-3 rounded-full font-semibold bg-black text-white">
+            Book
+          </motion.button>
+          <motion.button initial="hidden" whileInView="visible" viewport={{amount:0.8}} variants={buttonVariant} className="flex space-x-4 items-center">
+            {" "}
+            <span className="w-8 h-8 flex justify-center items-center bg-black text-white rounded-full ">
+              {" "}
+              <IoPlay />{" "}
+            </span>{" "}
+            <span>Watch Video</span>
+          </motion.button>
         </div>
+      </div>
+      <div className="w-full md:w-1/3">
+        <motion.img 
+        initial="hidden" 
+        whileInView="visible" 
+        viewport={{amount:0.8}} 
+        variants={imageVariant}
+          src="/hero.jpg"
+          alt="Card Image"
+          className="w-full h-70 object-cover rounded-full shadow-md "
+        />
+      </div>
     </div>
-    <div>
-    <img src="/hero.jpg" alt="Card Image" className="w-72 h-70 object-cover rounded-lg shadow-md" />
+  );
+};
 
-    </div>
-    </div>
-  )
-}
-
-export default Hero
+export default Hero;
--- src/components/Insights.jsx
+++ src/components/Insights.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import { div } from "framer-motion/client";
+import React from "react";
+import { SiDatabricks, SiGoogleassistant } from "react-icons/si";
 
 const Insights = () => {
-  return (
-    <div>Insights</div>
-  )
-}
+  const insightsData = [
+    {
+      title: "Productivity Enhancer",
+      date: "July 2023",
+      icon: SiGoogleassistant,
+      description: "Dyanmic visibility tools built to drive engagement",
+    },
 
-export default Insights
+    {
+      title: "Remote Assistance",
+      date: "Oct 2023",
+      icon: SiDatabricks,
+      description:
+        "Professional support for teams operating from remote location",
+    },
+  ];
+  return (
+    <div className="w-4/5 m-auto py-20 flex flex-col md:flex-row justify-between space-y-10 md:space-y-0">
+      <div className="md:w-1/3 space-y-4">
+        <h2 className="text-2xl font-bold">Industry Insights </h2>
+        <p>
+          The technology industry is driven by AI innovation, hybrid cloud
+          adoption, and a strong focus on cybersecurity with zero-trust models
+          to combat rising threats.
+        </p>
+        <div className="flex items-center space-x-2">
+          <img src="/men.png" alt="" className="w-12 h-12 rounded-full border-4" />
+          <div>
+            <h1 className="font-bold text-lg ">Michel Runner </h1>
+            <p className="text-sm text-gray-400">CEO, Founder</p>
+          </div>
+        </div>
+      </div>
+      <div className="md:w-1/2 flex flex-col md:flex-row md:space-x-5 md:space-y-0">
+        {insightsData.map((insights, index) => (
+          <div className="w-full p-4 border-4 border-white/50 rounded-xl space-y-4" key={index}>
+            <div>
+              <div>
+                <button>{insights.icon()}</button>
+                <h1>{insights.title}</h1>
+              </div>
+              <p>{insights.date}</p>
+            </div>
+            <p>{insights.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Insights;
